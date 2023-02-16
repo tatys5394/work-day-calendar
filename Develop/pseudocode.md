31aeee681c9a2b6c4f5fcc3a84b559ddffe91d7e
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-02-15 23:39:01 -0500

    first commit

---
+++ Develop/pseudocode.md
@@ -0,0 +1,20 @@
+currentTimeEl.style.visibility = "hidden";
+
+
+
+currentTimeEl.style.visibility = "hidden";
+
+var blockHour;
+var timeBlockEl = document.querySelector('#time-block')
+
+/ $('.time-block').each(function () {
+    if(blockHour < currentTime) {
+  blockHourEl.css('color', 'gray');
+
+    } else if (blockHour === currentTime) {
+  blockHour.css('color','red');
+
+    } else{
+  blockHour.css('color','green');
+    }
+  })
--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,32 @@
+// the current day is displayed at the top of the calendar 
+
+var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
+
+
+// I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future 
+
+var currentTime = today.getHours()
+$('#currentTime').text(today.format('MMM D, YYYY'));
+
+
+
+
+
+// $('');
+
+// $(document).ready(function () {
+//   var stringVar =“3”
+//   var numVar = parseInt(stringVar)
+//   console.log(stringVar == numVar);
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +45,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
